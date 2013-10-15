0f19dd79cb9b078e3d92a670b2a82e8581ad8004
johntday <johntday@gmail.com>
2013-10-15 05:25:27 -0500

    handlebars template for generating form input html

--- todo02/client/helpers/handlebars.js
+++ todo02/client/helpers/handlebars.js
@@ -1,3 +1,19 @@
+/*
+ Template to create input form elements
+ */
+Handlebars.registerHelper('form_input', function(name, value) {
+
+	return new Handlebars.SafeString(
+		"<div class='form-group row'>"+
+			"<label for='" + name + "' class='col-sm-2 control-label'>" + name + "</label>"+
+			"<div class='col-sm-6'>"+
+			"<input type='text' class='form-control' id='" + name + "' value='" + value + "' " + Meteor.MyClientModule.isReadOnly() + " />"+
+			"</div>"+
+			"</div>"
+	);
+});
+
+
 Handlebars.registerHelper('pluralize', function(n, thing) {
   // fairly stupid pluralizer
   if (n === 1) {
@@ -6,3 +22,4 @@ Handlebars.registerHelper('pluralize', function(n, thing) {
     return n + ' ' + thing + 's';
   }
 });
+
