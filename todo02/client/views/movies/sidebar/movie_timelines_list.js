3faaabce2fc649d391a425fb50a89d757ded65b8
johntday <johntday@gmail.com>
2013-12-04 21:23:40 -0600

    fix

--- todo02/client/views/movies/sidebar/movie_timelines_list.js
+++ todo02/client/views/movies/sidebar/movie_timelines_list.js
@@ -3,7 +3,8 @@ Template.tmpl_movie_tl_item.helpers({
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.tmpl_movie_tl_list.helpers({
 	movies: function() {
-		return MovieTimelines.find({}, findOptions(movieTimelineSort['click_cnt'], 5) );
+		//return MovieTimelines.find({}, findOptions(movieTimelineSort['click_cnt'], 5) );
+		return MovieTimelines.find({}, {sort: {click_cnt:-1}, limit:5});
 	},
 	isMoreMovies: function() {
 //		var cnt = Movies.find({favs: Meteor.userId()}).count();
